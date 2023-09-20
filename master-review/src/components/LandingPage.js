7bcf95e18b54b421375d73e909b9100f1f10e7af
VaLalent <108317198+VaLalent@users.noreply.github.com>
2023-09-20 17:44:33 +0700

    poster

--- master-review/src/components/LandingPage.js
+++ master-review/src/components/LandingPage.js
@@ -3,51 +3,28 @@ import React from "react";
 import Navbar from "./Navbar";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import Poster from "./Poster";
+import { postersData, responsive } from "./Data";
 
 function LandingPage() {
 
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-
+  const poster = postersData.map((poster) => {
     return (
-        <>
-            <Navbar />
-            <div>
-                <h1>Trending</h1>
-                <Carousel responsive={responsive}>
-                    <div className="poster">
-                        <img className="poster--image" src="https://live.staticflickr.com/300/19346893796_0bcda46777_z.jpg" alt="poster" />
-                        <h2>The Last of Us</h2>
-                    </div>
-                    <div className="poster">
-                        <img className="poster--image" src="https://live.staticflickr.com/300/19346893796_0bcda46777_z.jpg" alt="poster" />
-                        <h2>The Last of Us</h2>
-                    </div>
-                    <div className="poster">
-                        <img className="poster--image" src="https://live.staticflickr.com/300/19346893796_0bcda46777_z.jpg" alt="poster" />
-                        <h2>The Last of Us</h2>
-                    </div>
-                </Carousel>;
-            </div>
-        </>
-    );
+      <Poster name={poster.name} url={poster.imageUrl} describe={poster.describe} />
+    )
+  });
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Trending</h1>
+        <Carousel swipeable={false} draggable={false} showDots={true} responsive={responsive}>
+          {poster}
+        </Carousel>;
+      </div>
+    </>
+  );
 }
 
 export default LandingPage
